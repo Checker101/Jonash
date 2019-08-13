f493eefa254ad2789a59dbfe1b2a8597a25b8224
Mark Jonash Caraan <49311164+Checker101@users.noreply.github.com>
2019-08-13 19:00:16 +0800

    Add files via upload

---
+++ capital city.py
@@ -0,0 +1,9 @@
+word = "Ottawa"
+userentry = ""
+
+while  userentry != word:
+    userentry = input("What is the capital city of Canada?")
+    print ("Wrong, try again")
+else:
+    userentry = input("What is the capital city of Canada?")
+    print("Correct")
